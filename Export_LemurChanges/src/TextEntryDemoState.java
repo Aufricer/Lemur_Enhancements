08098cb5639380c32706ae408930a345b6a696d2
Aufricer <Aufricer@gmx.de>
2018-10-14 01:18:41 +0200

    Even more options for the textfield mouselistener

--- Export_LemurChanges/src/TextEntryDemoState.java
+++ Export_LemurChanges/src/TextEntryDemoState.java
@@ -341,13 +341,24 @@ public class TextEntryDemoState extends BaseAppState {
         private long zeit = System.currentTimeMillis();
 
 
-
         @Override
         public void mouseButtonEvent(MouseButtonEvent event, Spatial target, Spatial capture) {
             if (!(target instanceof TextField)) return;
             TextField textField = (TextField) target;
             DocumentModel document = textField.getDocumentModel();
             if (event.isPressed()) {
+
+                // on click + all selected = we empty the selections
+                if (document.getAnchors() != null) {
+                    if (document.getAnchors().size() ==1){
+                        int[] II = document.getAnchors().get(0);
+                        if ((II[0] == 0) && (II[1] == document.getfulltext().length()))  {
+                            document.emptyAnchors();
+                            zeit-=500;
+                        }
+                    }
+                }
+
                 int [] p = {event.getX(),event.getY()};
                 p =  textField.getTextlinesbyCoordinates(p);
                 // if we have a new position
@@ -360,12 +371,16 @@ public class TextEntryDemoState extends BaseAppState {
                 if (event.getButtonIndex() == 0)  press = true;
                 if (event.getButtonIndex() == 1) press2 = true;
 
+                // set Focus if we dont have it
+                if (textField !=  GuiGlobals.getInstance().getFocusManagerState().getFocus()) {
+                    GuiGlobals.getInstance().getFocusManagerState().setFocus(target);
+                }
+
+
             } else {
                 press = false;
                 press2 = false;
                 if (System.currentTimeMillis()-zeit < 500){ //Double click
-                    int [] p = {0,0};
-                    //  document.addTextselectArea(0,document.getoffsetText(p,false).length());
                     document.addTextselectArea(0,document.getfulltext().length());
                 }
                 zeit = System.currentTimeMillis();
