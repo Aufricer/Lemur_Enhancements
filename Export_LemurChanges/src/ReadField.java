7e6fef1a6095780b681225e551a7ea3d50df24e7
Aufricer <Aufricer@gmx.de>
2022-04-30 00:21:16 +0200

    Time again to work on ReadFields. -> minor changes Added a new ReadFieldDragger that basicaly does what Draghandler does but also resizes the element

--- Export_LemurChanges/src/ReadField.java
+++ Export_LemurChanges/src/ReadField.java
@@ -1,5 +1,3 @@
-package Elemente;
-
 import ViewPortPanel.ViewportPanel;
 import com.jme3.app.state.AppStateManager;
 import com.jme3.font.BitmapText;
@@ -23,7 +21,9 @@ import com.simsilica.lemur.core.VersionedReference;
 import com.simsilica.lemur.event.*;
 import com.simsilica.lemur.style.*;
 import com.simsilica.lemur.text.DocumentModel;
+
 import java.util.concurrent.Callable;
+
 import static com.simsilica.lemur.component.TextEntryComponent.STRC;
 
 public class ReadField extends Panel {
@@ -70,13 +70,14 @@ public class ReadField extends Panel {
 
     private Textfieldselectors selector;
 
+
     // only text
     public ReadField(String style, String text) {
         this(true,new ElementId(ELEMENT_ID),style,text,null,false,null,null,null);
     }
 
     public ReadField(String text) {
-        this(true,new ElementId(ELEMENT_ID),null,text,null,false,null,null,null);
+         this(true,new ElementId(ELEMENT_ID),null,text,null,false,null,null,null);
     }
 
     // text and headline
@@ -91,7 +92,7 @@ public class ReadField extends Panel {
     // text, optional headline and optional scene
     public ReadField(String style, String text, String headline, boolean showheadline, Spatial scene, AppStateManager manager) {
         this(true,new ElementId(ELEMENT_ID),style,text,headline,showheadline,manager,scene,null);
-    }
+     }
 
     public ReadField( String text, String headline, boolean showheadline, Spatial scene, AppStateManager manager) {
         this(true,new ElementId(ELEMENT_ID),null,text,headline,showheadline,manager,scene,null);
@@ -99,7 +100,7 @@ public class ReadField extends Panel {
 
     // text, optional headline and optional picture
     public ReadField(String style, String text, String headline, boolean showheadline, Texture image) {
-        this(true,new ElementId(ELEMENT_ID),style,text,headline,showheadline,null,null,image);
+          this(true,new ElementId(ELEMENT_ID),style,text,headline,showheadline,null,null,image);
     }
 
     public ReadField(String text, String headline, boolean showheadline, Texture image) {
@@ -116,13 +117,13 @@ public class ReadField extends Panel {
         this.preferredshowheadline = showhead;
         showheadline = showhead;
 
-        // We create 2 containers for headline + image or scene and text
+    // We create 2 containers for headline + image or scene and text
         headlineContainer = new Container(new SpringGridLayout(Axis.Y,Axis.X,FillMode.None,FillMode.None),new ElementId(Headline_ID).child(elementId).child(CONTAINER_ID), style);
         tfContainer = new Container(elementId.child(CONTAINER_ID), style);
         layout.addChild(headlineContainer,BorderLayout.Position.North);
         layout.addChild(tfContainer, BorderLayout.Position.Center);
 
-        // add textfield and selectors
+    // add textfield and selectors
         textField = new com.simsilica.lemur.TextField(text, elementId.child(TextField_ID), style);
         textField.adjustText(0,true,true);
         tfmodelref = textField.getDocumentModel().createReference();
@@ -131,6 +132,7 @@ public class ReadField extends Panel {
         selector = new Textfieldselectors();
         MouseEventControl.addListenersToSpatial(textField, selector);
         textField.setreadonly(true); // This can be set via style but to be sure we set it here again
+
 
         // prepare slider and reference
         baseIndex = new DefaultRangedValueModel();
@@ -162,14 +164,14 @@ public class ReadField extends Panel {
         preferredelementpicturewidth = elementwidth*0.8f;
 
 
-        // We prepare a ViewPortPanel to show
+          // We prepare a ViewPortPanel to show
         if (!(stateManager == null)) {
-            ElementId subid = new ElementId(ViewportPanel.ELEMENT_ID).child(ELEMENT_ID).child(Container.ELEMENT_ID);
-            VPscenebox = new ViewportPanel(stateManager, subid, style);
-            setViewPortPanelScene(scene);
-            preferredshowimage = true;
-            elementpictureheight=preferredelementpictureheight;
-            elementpicturewidth = preferredelementpicturewidth;
+          ElementId subid = new ElementId(ViewportPanel.ELEMENT_ID).child(ELEMENT_ID).child(Container.ELEMENT_ID);
+          VPscenebox = new ViewportPanel(stateManager, subid, style);
+          setViewPortPanelScene(scene);
+          preferredshowimage = true;
+          elementpictureheight=preferredelementpictureheight;
+          elementpicturewidth = preferredelementpicturewidth;
         }
         // We prepare a picture to show
         if (!(txtu == null)) {
@@ -184,7 +186,6 @@ public class ReadField extends Panel {
 
         }
 
-
         // apply our individual Readfield styles
         // those can be applied from inside this class or from a style
         if (applyStyles) {
@@ -194,7 +195,7 @@ public class ReadField extends Panel {
         showHeadline();
         showPictureorScene();
         updateallsizes();
-        //  textField.getDocumentModel().updateCarat(false, 0, true); // carat +0, um caratupdate -> resetcursorposition -> autoadjust textfield
+      //  textField.getDocumentModel().updateCarat(false, 0, true); // carat +0, um caratupdate -> resetcursorposition -> autoadjust textfield
         textField.getDocumentModel().end(false);
         textField.getDocumentModel().home(false);
     }
@@ -227,10 +228,27 @@ public class ReadField extends Panel {
             textField.getActionMap().remove(new KeyAction(KeyInput.KEY_C, KeyModifiers.CONTROL_DOWN), STRC);
         }
     }
+    public void enable_Dragging (boolean Dragable, AppStateManager lstateManager) {
+        enable_Dragging(Dragable,lstateManager,false);
+    }
 
-    public void enable_Dragging (boolean Dragable) {
+    public void enable_Dragging (boolean Dragable, AppStateManager lstateManager, boolean resizing) {
+        // unfortunately Dragging with Draghandler from com.simsilica.lemur.event only works for the container part of
+        // our readField. Thus we had to find a solution for the (optional) hedline and non optinal textField
+        // Thus we attach the custom made ReadFieldDragger.
+        // Note that the ReadFieldDragger needs the AppstateManager to choose and select the (mouse) cursor
+
+        ReadFieldDraggers dr = new ReadFieldDraggers(lstateManager,resizing);
         if (Dragable) {
-            CursorEventControl.addListenersToSpatial(headline, new DragHandler());
+         //   CursorEventControl.addListenersToSpatial(this, new DragHandler());
+            MouseEventControl.addListenersToSpatial(this,dr);
+            MouseEventControl.addListenersToSpatial(this.getTextfield(),dr);
+            MouseEventControl.addListenersToSpatial(this.getHeadline(),dr);
+        } else {
+            MouseEventControl.removeListenersFromSpatial(this,dr);
+            MouseEventControl.removeListenersFromSpatial(textField,dr);
+            MouseEventControl.removeListenersFromSpatial(headline,dr);
+
         }
     }
 
@@ -266,7 +284,7 @@ public class ReadField extends Panel {
         return textField;
     }
 
-    // styles.applyStyles(this, elementId.getId(), style);
+  // styles.applyStyles(this, elementId.getId(), style);
     @StyleDefaults(ELEMENT_ID)
     public static void initializeDefaultStyles(Styles styles, Attributes attrs) {
 /*      This function is called upon calling the styles.applyStyles function
@@ -287,8 +305,8 @@ public class ReadField extends Panel {
         attrs.set("setReadfieldimagewidth", 0f, false);
         attrs.set("setReadfieldimageheight", 0f, false);
         attrs.set("setReadField_keepOriginalImagesize_ratios", false, false);
-        //       attrs.set("showHeadline", true, true);
-    }
+ //       attrs.set("showHeadline", true, true);
+ }
 
     private void makeContainer(){
         // we delete all containers
@@ -330,11 +348,12 @@ public class ReadField extends Panel {
         width = Math.min(width,elementwidth);
         preferredelementpicturewidth = Math.max(0,width);
         if (!(VPscenebox== null))   VPscenebox.setPreferredSize(new Vector3f(preferredelementpicturewidth,elementpictureheight,1));
-        if (!(picture== null))
+        if (!(picture== null)) {
             if (scaleimage) {
-                preferredelementpictureheight = Math.min(preferredelementpicturewidth/getRatio(),elementheight);
+                preferredelementpictureheight = Math.min(preferredelementpicturewidth / getRatio(), elementheight);
             }
-        picture.setPreferredSize(new Vector3f(preferredelementpicturewidth,preferredelementpictureheight,1));
+            picture.setPreferredSize(new Vector3f(preferredelementpicturewidth, preferredelementpictureheight, 1));
+        }
         updateneed = true;
     }
 
@@ -379,7 +398,6 @@ public class ReadField extends Panel {
     public void setReadField_keepOriginalImagesize_ratios(boolean keep){
         scaleimage = keep;
     }
-
 
 
     private void setViewPortPanelScene(Spatial scene) {
@@ -459,7 +477,7 @@ public class ReadField extends Panel {
                 if (!(X == null))   headlineContainer.removeChild(X);
                 updateneed = true;
             }
-            return;
+        return;
         }
         if (!(picture==null)) { // picture
             if ((showimage) && (picture.getParent() == null)) { // not attached picture
@@ -530,36 +548,36 @@ public class ReadField extends Panel {
 
         // check 01 - our ReadField needs at least one line, to not throw an exception
         if ((elementheight-headlineheight-middleheight) <= lineheight) {
-            middleheight = Math.max(elementheight -headlineheight-lineheight,0); // make scene smaller down to 0
-            if (((elementheight-headlineheight-middleheight) <= lineheight) && (preferredshowheadline)) {
-                showheadline =false; // disable headline
-                headlineheight =0;
-                //      showHeadline();
-            }
-            if (elementheight <lineheight) elementheight = lineheight; // change elementheight
-        }  else {
+           middleheight = Math.max(elementheight -headlineheight-lineheight,0); // make scene smaller down to 0
+           if (((elementheight-headlineheight-middleheight) <= lineheight) && (preferredshowheadline)) {
+               showheadline =false; // disable headline
+               headlineheight =0;
+         //      showHeadline();
+           }
+           if (elementheight <lineheight) elementheight = lineheight; // change elementheight
+       }  else {
             // check 02 - if there is more height then 1 line we check for the size of headline and scene
             // headline has priority
-            float heightforscene =  elementheight  - numberofvisiblelines*lineheight - headlineheight;
+           float heightforscene =  elementheight  - numberofvisiblelines*lineheight - headlineheight;
             if (heightforscene > preferredelementpictureheight) {
-                middleheight = preferredelementpictureheight;
-            } else {
-                // we need to reduce the height of the scene or if thats not enough even the headline
-                float reducelines = (preferredelementpictureheight - heightforscene)/lineheight;
-                reducelines = (float) Math.ceil(reducelines);
-                reducelines = Math.min(reducelines,numberofvisiblelines-1);
-                heightforscene = elementheight  - (numberofvisiblelines-reducelines)*lineheight - headlineheight;
-                if (heightforscene > 0) {
-                    middleheight = heightforscene;
-                } else {
-                    // we remove the headline (and save the height if there was some at all)
-                    showheadline = false;
-                    //     showHeadline();
-                    heightforscene += headlineheight;
-                    headlineheight = 0;
-                    middleheight = heightforscene; // we could remove the scene as well as it might be super small but we dont know how big the headline would be
-                }
-            }
+               middleheight = preferredelementpictureheight;
+           } else {
+               // we need to reduce the height of the scene or if thats not enough even the headline
+               float reducelines = (preferredelementpictureheight - heightforscene)/lineheight;
+               reducelines = (float) Math.ceil(reducelines);
+               reducelines = Math.min(reducelines,numberofvisiblelines-1);
+               heightforscene = elementheight  - (numberofvisiblelines-reducelines)*lineheight - headlineheight;
+               if (heightforscene > 0) {
+                   middleheight = heightforscene;
+               } else {
+                   // we remove the headline (and save the height if there was some at all)
+                   showheadline = false;
+              //     showHeadline();
+                   heightforscene += headlineheight;
+                   headlineheight = 0;
+                   middleheight = heightforscene; // we could remove the scene as well as it might be super small but we dont know how big the headline would be
+               }
+           }
         }
         showHeadline();
 
@@ -657,7 +675,7 @@ public class ReadField extends Panel {
         if (Math.max(1, textField.getDocumentModel().getLineCount() - 1) ==  baseIndex.getMaximum())   return false;
         baseIndex.setMaximum(Math.max(1, textField.getDocumentModel().getLineCount() - 1));
         return true;
-    }
+       }
 
     private void updateslidervalue() {
         lastcaratline = textField.getDocumentModel().getCaratLine();
@@ -671,19 +689,20 @@ public class ReadField extends Panel {
         sliderref.update();
     }
 
+
     @Override
     public void updateLogicalState(float tpf) {
-        try {
+         try {
             super.updateLogicalState(tpf);
         } catch (IllegalArgumentException e) {
-            //  as the user can set sizes for the elements of the ReadField (the scene, the picture, the TextField, generell sizes)
-            //  it is possible that in the update loop of controls in BorderLayout.class line 61 reshape() a negative size
-            //  is calculated. Thus is thrown in Guicontroll.class setsize() line 278/310 //BorderLayout Class Line 68 and 101
-            // unfortunately I could not find a way to calculate the minimum size of the elements
-            // thus we take the error message that is giving us the calculated negative (or missing) size
-            // this size we finally add to our minimum ReadField sizes that are used in updateallsizes()
+          //  as the user can set sizes for the elements of the ReadField (the scene, the picture, the TextField, generell sizes)
+          //  it is possible that in the update loop of controls in BorderLayout.class line 61 reshape() a negative size
+          //  is calculated. Thus is thrown in Guicontroll.class setsize() line 278/310 //BorderLayout Class Line 68 and 101
+          // unfortunately I could not find a way to calculate the minimum size of the elements
+          // thus we take the error message that is giving us the calculated negative (or missing) size
+          // this size we finally add to our minimum ReadField sizes that are used in updateallsizes()
 
-            //  System.out.println("Error" + e);
+          //  System.out.println("Error" + e);
             int pos = e.toString().indexOf("(")+1;
             int pos2 = e.toString().indexOf(",", pos);
             float xadd = (float) Float.parseFloat(e.toString().substring(pos,pos2));
@@ -709,7 +728,7 @@ public class ReadField extends Panel {
                 updateslidermax();
                 updateslidervalue();
             }
-        }
+         }
 
         if (sliderref.update()) {
             /* pls note that this wont work correctly if sliderbehaviour_normal = false.
@@ -724,7 +743,7 @@ public class ReadField extends Panel {
             Integer it = textField.getDocumentModel().findCaratValue(positionen);
             if (!(it == null)) textField.getDocumentModel().updateCarat(true, it, true); // Setze Carat an die neue Position
             textField.getDocumentModel().findPosition(textField.getDocumentModel().getCarat(), positionen);
-            GuiGlobals.getInstance().getFocusManagerState().setFocus(textField);
+             GuiGlobals.getInstance().getFocusManagerState().setFocus(textField);
             // ermittle das neue Y-Offset und setze es
             textField.getDocumentModel().setOffset_Y(textField.getDocumentModel().getCaratLine() - textField.getPreferredLineCount()+1);
         }
@@ -738,3 +757,5 @@ public class ReadField extends Panel {
 
     }
 }
+
+
---
+++ Export_LemurChanges/src/ReadFieldDraggers.java
@@ -0,0 +1,173 @@
+import Editor.Config_AppState;
+import com.jme3.app.state.AppStateManager;
+import com.jme3.bounding.BoundingBox;
+import com.jme3.input.event.MouseButtonEvent;
+import com.jme3.input.event.MouseMotionEvent;
+import com.jme3.math.Vector2f;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Spatial;
+import com.simsilica.lemur.TextField;
+import com.simsilica.lemur.event.DefaultMouseListener;
+import org.lwjgl.opengl.Display;
+
+public class ReadFieldDraggers extends DefaultMouseListener{
+    // A copy of Draghandler() more or less
+    // Detects clicks and movements on a readField
+    // Chooses if the element has to be moved or resized, changing cursor accordingly
+    // If resizing is allowed at all, a double click will scale back the element to its normal
+    // dimensions in two steps (1st make x and y scale equal, 2nd scale back to 100 %)
+    // works with right mouse click
+
+    private Boolean Klick =false;
+    private int Taste;
+    private Vector2f start;
+    private Vector2f start2;
+    private long time;
+    private long time2;
+    private AppStateManager localstateManager;
+    private boolean resize;
+
+    public ReadFieldDraggers(AppStateManager manager) {
+        this(manager,false);
+        }
+
+    public ReadFieldDraggers (AppStateManager manager, boolean allow_resizing){
+        localstateManager = manager;
+        this.resize = allow_resizing;
+    }
+
+    int counter = 0;
+    @Override
+    protected void click(MouseButtonEvent event, Spatial target, Spatial capture) {
+       if  (Taste == 1 && System.currentTimeMillis() - time < 500) { // Kurzer Klick
+           counter++;
+       }
+       if (counter == 1)   time2 = System.currentTimeMillis();
+       if (counter == 2) {
+           // Doppelklick ?
+           if (((time - time2) < 600) && resize){
+               Spatial move = target;
+               if (target.getClass().equals(TextField.class)) move = move.getParent().getParent();
+               float x = ((Elemente.ReadField) move).getLocalScale().x;
+               float y = ((Elemente.ReadField) move).getLocalScale().y;
+               if (x == y) { //Skalierung des Elements in 2 Schüben normalisieren
+                   ((Elemente.ReadField) move).setLocalScale(1f,1f,1f);
+               } else {
+                   x = Math.max(x,y);
+                   ((Elemente.ReadField) move).setLocalScale(x,x,1f);
+               }
+           }
+           counter = 0;
+       }
+
+    }
+
+      @Override
+    protected boolean isClick(MouseButtonEvent event, int xDown, int yDown) {
+          return super.isClick(event, xDown, yDown);
+    }
+
+
+    int i =0;
+    int Klicktyp = 0;
+    @Override
+    public void mouseButtonEvent(MouseButtonEvent event, Spatial target, Spatial capture) {
+        Klick = event.isPressed();
+        Taste = event.getButtonIndex();
+        if (Klick) {
+            this.start = new Vector2f(event.getX(),event.getY());
+            this.time =System.currentTimeMillis();
+            i=1;
+            Klicktyp = 0;
+            start2 = start.clone();
+        } else {
+            localstateManager.getState(Config_AppState.class).MouseCursorChooser(Config_AppState.MouseCursorTypes.Standard);
+            if (isClick(event,(int) start2.getX(), (int) start2.getY())) {
+                click(event,target,capture);
+            }
+        }
+    }
+
+    @Override
+    public void mouseEntered(MouseMotionEvent event, Spatial target, Spatial capture) {
+        super.mouseEntered(event, target, capture);
+    }
+
+    @Override
+    public void mouseExited(MouseMotionEvent event, Spatial target, Spatial capture) {
+        super.mouseExited(event, target, capture);
+    }
+
+    @Override
+    public void mouseMoved(MouseMotionEvent event, Spatial target, Spatial capture) {
+        // System.out.println("Mousebewegung " + event.getX());
+        event.setConsumed();
+        super.mouseMoved(event, target, capture);
+        // Rechtsklick und etwas Verzögerung
+        if (Klick && Taste == 1 && System.currentTimeMillis() - time > 750) {
+            Vector2f current = new Vector2f(event.getX(), event.getY());
+            Vector2f delta = current.subtract(start);
+            Spatial move = target;
+
+            if (target.getClass().equals(TextField.class)) {
+                // headline oder textField -> parent ist der Container, parent davon das ReadField
+                move = move.getParent().getParent();
+            }
+
+            Elemente.ReadField rFtemp = (Elemente.ReadField) move;
+
+            Vector2f Maße = new Vector2f();
+            BoundingBox b = (BoundingBox) rFtemp.getWorldBound();
+
+            Maße.x = b.getXExtent(); // aktuelle Breite inkl. Skalierung
+            Maße.y = b.getYExtent(); // aktuelle Höhe, inkl. Skalierung
+
+            double minx = 1-Math.max(10/(Maße.x * 2 ), 0.04);     //Mindestens 10 oder 4 %
+            double miny = Math.max(10/ (Maße.y*2),0.04);
+            if ((event.getX() > (rFtemp.getLocalTranslation().x + Maße.x * 2 * minx) && (event.getY() > rFtemp.getLocalTranslation().y - Maße.y * 2 *miny)) && resize) {
+               //Skalieren, aber nur wenn Resize erlaubt ist
+                if (i == 1) {
+                    localstateManager.getState(Config_AppState.class).MouseCursorChooser(Config_AppState.MouseCursorTypes.Diagonal_right_oriented);
+                    i = 0;
+                    Klicktyp = 1;
+                }
+                if (!(Klicktyp == 1)) return;
+                // Beim Skalieren ändert sich die Y Position sowie die Größe des Feldes in X und Y Richtung
+                float x_in_rF = start.getY()-(rFtemp.getLocalTranslation().y - Maße.y*2);
+                float x2_in_rF = event.getY()-(rFtemp.getLocalTranslation().y - Maße.y*2);
+                float l2 = (Maße.y*2)/(x_in_rF/x2_in_rF);  // stauchung = (x_in_rF/x2_in_rF);, Differenz der Länge zur akteullen y pos (l2 - (Maße.y*2)
+             //   float normal = (Maße.y*2)/rFtemp.getLocalScale().y;
+                float neuscale_y = l2/((Maße.y*2)/rFtemp.getLocalScale().y);
+             //    neue Breite  = event.getX() + delta.getX() - rFtemp.getLocalTranslation().x)
+             //    alte Breite = (event.getX() - rFtemp.getLocalTranslation().x)
+
+                if (rFtemp.getLocalScale().x * (event.getX()+delta.getX()-rFtemp.getLocalTranslation().x)/(event.getX() -rFtemp.getLocalTranslation().x) > 0.25 && neuscale_y >= 0.25) {
+                    rFtemp.setLocalScale(rFtemp.getLocalScale().x * (event.getX() + delta.getX() - rFtemp.getLocalTranslation().x) / (event.getX() - rFtemp.getLocalTranslation().x), neuscale_y, 1f);
+                    rFtemp.setLocalTranslation(rFtemp.getLocalTranslation().addLocal(0f,l2-(Maße.y)*2,0f));
+                }
+           start = current;
+
+            }else { // Bewegen, wenn nicht resized wird
+               if (i == 1) {
+                  localstateManager.getState(Config_AppState.class).MouseCursorChooser(Config_AppState.MouseCursorTypes.Move);
+                  i = 0;
+                   Klicktyp = 2;
+               }
+               if (!(Klicktyp == 2)) return;
+               Vector3f zielposition = null;
+               zielposition = move.getLocalTranslation().addLocal(new Vector3f(delta.x, delta.y, 0f));
+               zielposition.setX(Math.min(Display.getWidth() - Maße.x, zielposition.x)); // halbes Feld außerhalb erlaubt
+               zielposition.setX(Math.max(-Maße.x, zielposition.x));
+               zielposition.setY(Math.min(Display.getHeight(), zielposition.y)); // Rahmen oben
+               zielposition.setY(Math.max(Maße.y, zielposition.y)); //halbes Feld nach unten
+
+               move.setLocalTranslation(zielposition);
+               start = current;
+            }
+
+
+            }
+        }
+
+
+    }
